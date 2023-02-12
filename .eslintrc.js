b26c7fbead38495045c7481502a8dc3da797f769
esra akbulat <esra@klinikya.com>
2023-02-12 14:41:49 +0300

    add tailwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,33 +6,32 @@ module.exports = {
     es2021: true,
   },
 
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
 
   overrides: [],
-  parser: 'babel-eslint',
+
+  parser: '@typescript-eslint/parser',
+
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
+    requireConfigFile: false,
     ecmaFeatures: {
       jsx: true,
       modules: true,
       experimentalObjectRestSpread: true,
     },
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react'],
 
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        parser: 'flow',
-      },
-    ],
+    'prettier/prettier': 'error',
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     singleQuote: true,
+    //     parser: 'flow'
+    //   }
+    // ]
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import Navbar from './components/navbar';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
